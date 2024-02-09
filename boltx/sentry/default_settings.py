e13f3c09d631ed5b8868f405616d2cc30b9526d9
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-02-09 13:06:57 -0600

    Use default BOLT_ env setting behavior for some settings

--- boltx/sentry/default_settings.py
+++ boltx/sentry/default_settings.py
@@ -1,24 +1,16 @@
 from os import environ
 
-SENTRY_AUTO_INIT: bool = environ.get("SENTRY_AUTO_INIT", "true").lower() in (
-    "true",
-    "1",
-    "yes",
-)
+SENTRY_AUTO_INIT: bool = True
 SENTRY_INIT_KWARGS: dict = {}
+SENTRY_PII_ENABLED: bool = True
 
+# Re-use the standard Sentry env vars
 SENTRY_DSN: str = environ.get("SENTRY_DSN", "")
 SENTRY_RELEASE: str = environ.get("SENTRY_RELEASE", "")
 SENTRY_ENVIRONMENT: str = environ.get("SENTRY_ENVIRONMENT", "production")
 
+# These aren't built in as Sentry env vars?
 SENTRY_TRACES_SAMPLE_RATE: float = float(environ.get("SENTRY_TRACES_SAMPLE_RATE", 0.0))
-
 SENTRY_PROFILES_SAMPLE_RATE: float = float(
     environ.get("SENTRY_PROFILES_SAMPLE_RATE", 0.0)
-)
-
-SENTRY_PII_ENABLED: bool = environ.get("SENTRY_PII_ENABLED", "true").lower() in (
-    "true",
-    "1",
-    "yes",
 )
